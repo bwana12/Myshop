19a41e5e62b5664266f403fa71f0e0a05dccc621
bwana12 <onganimwase1@gmail.com>
2026-01-25 01:06:34 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -4,14 +4,24 @@ import {
   enableIndexedDbPersistence 
 } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-const firebaseConfig = {
-  apiKey: "...",
-  authDomain: "...",
-  projectId: "...",
+onst firebaseConfig = {
+  apiKey: "AIzaSyDwhyU5BrtjRhy-45rd5oYfzGO8abehUiM",
+  authDomain: "shopeasy-14890.firebaseapp.com",
+  projectId: "shopeasy-14890",
+  storageBucket: "shopeasy-14890.firebasestorage.app",
+  messagingSenderId: "45827037951",
+  appId: "1:45827037951:web:545d41703780fa51018aca",
+  measurementId: "G-BFPV2QQ80S"
 };
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+
+onSnapshot(collection(db, "products"), (snapshot) => {
+  snapshot.docs.forEach(doc => {
+    console.log(doc.data());
+  });
+});
 
 // ✅ ENABLE OFFLINE MODE
 enableIndexedDbPersistence(db).catch((err) => {
