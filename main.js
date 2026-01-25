351fd536bd679a4be0a33269d4da77ceebceaa54
bwana12 <onganimwase1@gmail.com>
2026-01-25 12:10:15 +0200

    Delete main.js

--- main.js
+++
@@ -1,5 +0,0 @@
-import { db } from "./firebase.js";
-import { collection, getDocs } from 
-"https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
-
-const querySnapshot = await getDocs(collection(db, "products"));
